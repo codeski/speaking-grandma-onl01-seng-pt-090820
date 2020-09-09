20140dcf9279f4f46a240b8f38b0cd299cbed81e
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-09 04:00:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 
-def speak_to_grandma(phrase)
+def speak_to_grandma()
   if "#{phrase}" == "I LOVE YOU GRANDMA!"
   print "I LOVE YOU TOO PUMPKIN!"
   elsif "#{phrase}" == "#{phrase}".upcase
