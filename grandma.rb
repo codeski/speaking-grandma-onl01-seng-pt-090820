7779a492fd8a1189a4b8350b37c3c73063aadcf5
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-09 04:16:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,11 @@
 
 def speak_to_grandma(phrase)
   if "#{phrase}" == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
+  return "I LOVE YOU TOO PUMPKIN!"
   elsif "#{phrase}" == "#{phrase}".upcase
-  print "NO, NOT SINCE 1938!"
+  return "NO, NOT SINCE 1938!"
   elsif "#{phrase}"
-  print "HUH?! SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
